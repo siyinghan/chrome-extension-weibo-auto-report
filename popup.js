9a8abfd8aea4c06d449c5d01a1418ed978018620
Siying Han <siying.han@ing.com>
2022-11-28 12:08:21 +0100

    v0.0.2

--- popup.js
+++ popup.js
@@ -48,8 +48,43 @@ function startTogeher1() {
   });
 }
 
-// Reload and report 宣扬仇恨, 造谣爆料 and 诱导不良行为 orderly.
+// Reload and report 宣扬仇恨, 造谣爆料 and 邪教 orderly.
 function startTogeher2() {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.reload(tabs[0].id);
+    setTimeout(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["scripts/xych.js"],
+      });
+    }, 1000);
+  });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    setTimeout(() => {
+      chrome.tabs.reload(tabs[0].id);
+    }, 3000);
+    setTimeout(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["scripts/zybl.js"],
+      });
+    }, 4000);
+  });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    setTimeout(() => {
+      chrome.tabs.reload(tabs[0].id);
+    }, 6000);
+    setTimeout(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ["scripts/xj.js"],
+      });
+    }, 7000);
+  });
+}
+
+// Reload and report 宣扬仇恨, 造谣爆料 and 诱导不良行为 orderly.
+function startTogeher3() {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.reload(tabs[0].id);
     setTimeout(() => {
@@ -84,4 +119,6 @@ function startTogeher2() {
 }
 
 document.querySelector("#wrmm").addEventListener("click", startWRMM);
-document.querySelector("#together").addEventListener("click", startTogeher2);
+document.querySelector("#together1").addEventListener("click", startTogeher1);
+document.querySelector("#together2").addEventListener("click", startTogeher2);
+document.querySelector("#together3").addEventListener("click", startTogeher3);
